a193d4f15c0afa37eda34031a99f78f66f103a64
derva1 <vdervisevi1@etf.unsa.ba>
2020-11-03 17:25:05 +0100

    Dodavanje konstruktora u klasi Interval

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,34 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
+    double pocetnaTacka;
+    double krajnjaTacka;
+    boolean pTacka, kTacka;
 
+    public Interval(double pocetnaTacka, double krajnjaTacka, boolean pTacka, boolean kTacka) {
+        if(pocetnaTacka > krajnjaTacka) throw new IllegalArgumentException("Pocetna tacka je veca od kranje");
+        this.pocetnaTacka = pocetnaTacka; this.krajnjaTacka = krajnjaTacka; this.pTacka = pTacka; this. kTacka = kTacka;
 
+    }
+    public Interval(){
+        pocetnaTacka = 0 ;
+        krajnjaTacka =0;
+        pTacka = false;
+        kTacka = false;
+    }
+    public static Interval intersect(Interval i, Interval i2) {
+        return new Interval(0,0,false,false);
+    }
+
+    public boolean isIn(double v) {
+        return  false;
+    }
+
+    public boolean isNull() {
+        return  false;
+    }
+
+    public static Interval intersect(Interval interval) {
+        return new Interval(0,0,false,false);
+    }
 }
